cb84a268e9412e13cbcceca9538530e378f21b12
xOcarin <119756949+xOcarin@users.noreply.github.com>
2023-04-18 20:35:54 -0400

    homepage
    -added php handlers and homescreen

---
+++ scripts/utils.js
@@ -0,0 +1,28 @@
+
+
+
+function showLogin() {
+  document.getElementById('login-container').style.display = 'block';
+  document.getElementById('leftID').style.display = 'none';
+  document.getElementById('rightID').style.display = 'none';
+}
+
+
+function showSignup() {
+  document.getElementById('signup-container').style.display = 'block';
+  document.getElementById('leftID').style.display = 'none';
+  document.getElementById('rightID').style.display = 'none';
+
+}
+
+function showBackButton() {
+  document.getElementById('backbutton').style.display = 'block';
+}
+
+function goBack() {
+  document.getElementById('backbutton').style.display = 'none';
+  document.getElementById('leftID').style.display = 'block';
+  document.getElementById('rightID').style.display = 'block';
+  document.getElementById('login-container').style.display = 'none';
+  document.getElementById('signup-container').style.display = 'none';
+}
