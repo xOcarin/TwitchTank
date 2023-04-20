75269ff190873b07a9405ed36b935e0483128540
xOcarin <119756949+xOcarin@users.noreply.github.com>
2023-04-20 01:59:46 -0400

    small electron tweaks

--- main.js
+++ main.js
@@ -9,13 +9,20 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1260,
     height: 720,
+    title: 'Twitch Tank',
+    icon: path.join(__dirname, '/assets/icon.png'),
     webPreferences: {
       nodeIntegration: true
     }
   });
 
-
   mainWindow.maximize();
+
+  // Remove default menus on macOS and Windows
+  if (process.platform === 'darwin' || process.platform === 'win32') {
+    const { Menu } = require('electron');
+    Menu.setApplicationMenu(null);
+  }
 
   staticServer.listen(3000, () => {
     console.log('Static server running on port 3000');
@@ -26,6 +33,7 @@ function createWindow() {
     console.log('API server running on port 8000');
   });
 }
+
 
 app.whenReady().then(() => {
   createWindow();
