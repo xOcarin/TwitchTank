d11deccae407fb580537c46eb0fb5ef4bda85435
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2023-04-22 15:21:14 -0400

    logout maybe?

---
+++ phphandler/logout.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// If the session was created with a cookie, remove the cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+}
+
+// Destroy the session
+session_destroy();
+
+// Redirect the user to the login page
+header("Location: http://localhost:3000/index.html");
+exit();
+?>
